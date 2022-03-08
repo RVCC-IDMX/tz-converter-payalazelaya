4a470ab010b72212ca7dc5cc8ff44192ed76cfe5
Paola Ayala <paolaaz235@gmail.com>
2022-03-08 13:23:50 -0500

    Begin command line
    Write the initial code for the timezone converter

---
+++ tz.js
@@ -0,0 +1,20 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-unused-expressions */
+const moment = require('moment-timezone');
+
+moment.tz.setDefault('America/New_York');
+
+let targetTimezone;
+
+if (process.argv.length !== [3]) {
+  console.log('Usage: node <script-file> <timezone>');
+  process.exit[1];
+} else {
+  targetTimezone = process.argv[2];
+}
+
+console.log(
+  `The time at the ${targetTimezone} timezone is ${moment()
+    .tz(targetTimezone)
+    .format()}`
+);
