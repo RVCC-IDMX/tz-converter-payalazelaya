a9d244a5193b67bb61a10e65c8d2c0a2393cb86c
Paola Ayala <paolaaz235@gmail.com>
2022-03-09 18:26:47 -0500

    Update with new date format

--- tz.js
+++ tz.js
@@ -4,19 +4,35 @@ const moment = require('moment-timezone');
 
 moment.tz.setDefault('America/New_York');
 
+const form = 'dddd, MMMM Do YYYY, h: mm:ss a';
+
 const yargs = require('yargs');
 
 let targetTimezone;
 
-if (process.argv.length !== [3]) {
+if (yargs.argv.length !== [3]) {
   console.log('Usage: node <script-file> <timezone>');
   process.exit[1];
 } else {
   targetTimezone = yargs.argv._[0];
 }
 
-console.log(
-  `The time at the ${targetTimezone} timezone is ${moment()
-    .tz(targetTimezone)
-    .format()}`
-);
+if (yargs.argv.format === true) {
+  console.log(
+    `The time at ${targetTimezone} timezone is ${moment()
+      .tz(targetTimezone)
+      .format('dddd, MMMM Do YYYY, h:mm:ss a')}`
+  );
+  console.log(
+    `The time right now is at America/New_York timezone is ${moment().format(
+      form
+    )}`
+  );
+} else {
+  console.log(
+    `The time at ${targetTimezone} timezone is ${moment()
+      .tz(targetTimezone)
+      .format()}`
+  );
+  console.log(`The time at America/New_York timezone is ${moment().format()}`);
+}
