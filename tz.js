c894d507bd6f69b02039e5f726c2b205de454cd1
Paola Ayala <paolaaz235@gmail.com>
2022-03-09 18:09:47 -0500

    Modify code with yargs
    Make change to add yargs to code

--- tz.js
+++ tz.js
@@ -4,13 +4,15 @@ const moment = require('moment-timezone');
 
 moment.tz.setDefault('America/New_York');
 
+const yargs = require('yargs');
+
 let targetTimezone;
 
 if (process.argv.length !== [3]) {
   console.log('Usage: node <script-file> <timezone>');
   process.exit[1];
 } else {
-  targetTimezone = process.argv[2];
+  targetTimezone = yargs.argv._[0];
 }
 
 console.log(
